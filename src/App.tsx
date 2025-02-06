421def62edc3c86bfe09923582cfebb2098fdd99
Mor-ald <mor.ald@yandex.ru>
2025-02-06 14:20:05 +0300

    feat: add Canvas, Control components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import { Tool } from './types/types';
+
+import { useRef, useState } from 'react';
+import { Stage } from 'konva/lib/Stage';
+
+import Canvas from '@/components/ui/canvas/Canvas';
+import Control from '@/components/ui/control/Control';
+
 const App = () => {
-  return <></>;
+  const [tool, setTool] = useState<Tool>('cursor');
+  const stageRef = useRef<Stage>(null);
+
+  return (
+    <>
+      <Canvas tool={tool} stageRef={stageRef} />
+      <Control tool={tool} setTool={setTool} />
+    </>
+  );
 };
 
 export default App;
